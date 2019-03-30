bb7ef1926be255852357887d901ab48db765b8ed
tigerbrothers <49062997+tigerbrothers@users.noreply.github.com>
2019-03-30 19:33:22 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+$(function(){
+ $("#decide").click(function(){
+  var flag = 0;
+  $(".errorbg").removeClass(".errorbg");
+  $(".errorcol").remove();
+  $(".imp").each(function(){
+   if($(this).val() === ""){
+    flag -= 1;
+    $(this).addClass("errorbg").after('<span class="errorcol">※</span>');
+   }
+  });
+
+  if(flag < 0){
+   $("#error").text("※どこか記入していない箇所があります").addClass("errorcol");
+  }else{
+   $("#question-area").fadeOut(500);
+   $("h1").fadeOut(500,function(){$("h1").text("正解した数は！？");});
+   $("h1").fadeIn(500);
+  }
+ });
+});
